3e555ad764002b06a806d52d4b75378baddb50ac
MarianoRuiz1 <marianojavierruiz01@gmail.com>
2023-11-21 20:34:57 -0300

    Clase 10

--- CoderApp/models.py
+++ CoderApp/models.py
@@ -5,4 +5,3 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion = models.CharField(max_length=100)
